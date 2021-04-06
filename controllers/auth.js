6f0a4b7fb14bfd35b058ee28eb8fe1c63713f823
YiroYi <yiroyi@live.com>
2021-04-06 00:42:19 -0500

    Add: Implementing authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,17 +2,31 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    errorMessage: req.flash('error')
+    errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error')
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
+    errorMessage: message
   });
 };
 
@@ -29,6 +43,7 @@ exports.postLogin = (req, res, next) => {
       bcrypt.compare(password, user.password)
       .then(doMatch => {
         if (!doMatch) {
+          req.flash('error', 'Invalid credentials');
           return res.redirect('/login');
         }
 
@@ -54,6 +69,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({email: email})
     .then(userDoc => {
       if(userDoc) {
+        req.flash('error', 'E-mail exists already');
         return res.redirect('/signup');
       }
 
