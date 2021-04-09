2ba0e51c6a1f9073cb5e225432af169b2ff3f5dd
YiroYi <yiroyi@live.com>
2021-04-08 23:05:01 -0500

    Add: SendGrid for sending mails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,16 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+const sendApiKey = process.env.API_KEY_SEND_GRID
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: sendApiKey
+  }
+}));
+
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error')
@@ -85,9 +96,20 @@ exports.postSignup = (req, res, next) => {
       })
       .then(result => {
         res.redirect('/login');
+        return transporter.sendMail({
+          to: email,
+          from: 'red.tokio@gmail.com',
+          subject: 'Signup succeeded!',
+          html: '<h1>You successfully signed up!</h1>'
+        });
       })
-    })
-    .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
 };
 
 exports.postLogout = (req, res, next) => {
