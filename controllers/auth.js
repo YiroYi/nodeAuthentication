32592fd8fd6319fd83962612e463c234b1625d96
YiroYi <yiroyi@live.com>
2021-04-10 13:21:42 -0500

    Add: Keeping user input in case of error

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,7 +14,6 @@ const transporter = nodemailer.createTransport(sendgridTransport({
   }
 }));
 
-
 exports.getLogin = (req, res, next) => {
   let message = req.flash('error')
   if (message.length > 0) {
@@ -40,13 +39,29 @@ exports.getSignup = (req, res, next) => {
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    errorMessage: message
+    errorMessage: message,
+    oldInput: {
+      email: '',
+      password: '' ,
+      confirmPassword: ''
+    }
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg
+    });
+  }
+
   User.findOne({email: email})
     .then(user => {
       if(!user) {
@@ -84,7 +99,12 @@ exports.postSignup = (req, res, next) => {
     return res.status(422).render('auth/signup', {
       path: '/signup',
       pageTitle: 'Signup',
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password ,
+        confirmPassword: req.body.confirmPassword
+      }
     });
   }
 
